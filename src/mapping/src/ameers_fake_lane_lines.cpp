36004f89d3e3fe4f9468fcc05af6fbf82615b199
ARezaK <ARezaK@users.noreply.github.com>
2014-12-14 22:19:59 -0500

    Update ameers_fake_lane_lines.cpp

--- src/mapping/src/ameers_fake_lane_lines.cpp
+++ src/mapping/src/ameers_fake_lane_lines.cpp
@@ -18,10 +18,10 @@
 
 
 sensor_msgs::Image image_used_for_ll;
+sensor_msgs::Image image_used_for_ll2;
 //***************************************End of global variables**************************************************************************************
 
 void convertcameraimage(const sensor_msgs::Image::ConstPtr& msg){
-    ROS_INFO_STREAM("GOT IMAGE");
     image_used_for_ll.header = msg->header;
     image_used_for_ll.height = msg->height;
     image_used_for_ll.width = msg->width;
@@ -29,14 +29,27 @@ void convertcameraimage(const sensor_msgs::Image::ConstPtr& msg){
     image_used_for_ll.is_bigendian = msg->is_bigendian;
     image_used_for_ll.step = msg->step;
     image_used_for_ll.data = msg->data;
+
+    //binary image same as above just binary
+    image_used_for_ll2.header = msg->header;
+    image_used_for_ll2.height = msg->height;
+    image_used_for_ll2.width = msg->width;
+    image_used_for_ll2.encoding = msg->encoding;
+    image_used_for_ll2.is_bigendian = msg->is_bigendian;
+    image_used_for_ll2.step = msg->step;
+    image_used_for_ll2.data = msg->data;
+
+
     int i = 0;
     int size = msg->step*msg->height;
     for(i=0;i<size-1;i++){
         if(msg->data[i]==0){
             image_used_for_ll.data[i]=255;
+            image_used_for_ll2.data[i]=255;
         }
         else{
             image_used_for_ll.data[i]=0;
+            image_used_for_ll2.data[i]=0;
         }
     }
 
@@ -62,12 +75,14 @@ int main(int argc, char **argv) {
 
     ros::Subscriber imageSub = n.subscribe("image",1,convertcameraimage);
     ros::Publisher imagePub = n.advertise<sensor_msgs::Image>("convertimage", 1); //Local1 map (lanelines)
+    ros::Publisher imagePub2 = n.advertise<sensor_msgs::Image>("visualizedimage", 1); //Local1 map (lanelines)
 
     ros::Rate rate(45);
 
     while(ros::ok()) {
 
         imagePub.publish(image_used_for_ll);
+        imagePub2.publish(image_used_for_ll2);
 
         ros::spinOnce();
         rate.sleep();
